9245d86cf01f2ad30c01a29f07665b88b837edf0
Sandevik <simon.sandevik@outlook.com>
2025-08-29 15:24:17 +0200

    bug fix

--- backend/src/blueprints/bookables/routes/update_bookable.rs
+++ backend/src/blueprints/bookables/routes/update_bookable.rs
@@ -5,12 +5,12 @@ use uuid::Uuid;
 use crate::{blueprints::{bookables::repo::{self, NewBookableDTO}, users::repo::UserProfile}, utils::api_response::ApiResponse};
 
 #[put("/{bookable_uuid}")]
-pub async fn route(req: HttpRequest, path: web::Path<(Uuid, Uuid)>, body: web::Json<NewBookableDTO>) -> impl Responder {
+pub async fn route(req: HttpRequest, path: web::Path<(Uuid,)>, body: web::Json<NewBookableDTO>) -> impl Responder {
     
     let user_profile = req.extensions().get::<UserProfile>().cloned().unwrap();
 
 
-    match repo::update_bookable(&user_profile, &path.1, &body.0).await {
+    match repo::update_bookable(&user_profile, &path.0, &body.0).await {
         Ok(up) => ApiResponse::Success { status_code: StatusCode::OK, data: up },
         Err(e) => ApiResponse::Error(e)
     }
